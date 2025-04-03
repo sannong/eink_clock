3efa439c8340cb54f29fd94056e9b6c2abe700da
Ben Sanda <sandab@shure.com>
2025-04-03 09:37:24 -0500

    Temperature added with weather icon printout and compressed date format.
    commit ff27c888a7188083c91cd24a31e52af1b7eba7f5
    Author: Ben Sanda <sandab@shure.com>
    Date:   Thu Apr 3 09:31:14 2025 -0500
    
        Added comments
    
    commit 571c1ad7745d53da157f07418424113311e6df67
    Author: Ben Sanda <bensanda@hulktek.com>
    Date:   Thu Apr 3 09:29:20 2025 -0500
    
        weather working now!
    
    commit 21047b1d34aba91794b9b0506bd8ff64ada26456
    Author: Ben Sanda <sandab@shure.com>
    Date:   Thu Apr 3 09:26:16 2025 -0500
    
        Adding weather icon
    
    commit 67f56b97c775b3395e0824ea7688b499a4706789
    Author: Ben Sanda <bensanda@hulktek.com>
    Date:   Thu Apr 3 09:18:59 2025 -0500
    
        temp working
    
    commit c67661153ad48a475f6c844b4879f2afa1dd8bbf
    Author: Ben Sanda <sandab@shure.com>
    Date:   Thu Apr 3 09:12:49 2025 -0500
    
        Temperature printout
    
    commit d5cf1e519761b831b05253b740029bdc3306a0af
    Author: Ben Sanda <bensanda@hulktek.com>
    Date:   Thu Apr 3 09:09:09 2025 -0500
    
        temp work fixing quotes
    
    commit bed75e30d436c03eb2c9a4bc2c88c50cbc2c6490
    Author: Ben Sanda <sandab@shure.com>
    Date:   Thu Apr 3 09:03:43 2025 -0500
    
        Starting to add weather data

--- eink.py
+++ eink.py
@@ -11,22 +11,55 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 print(libdir)
 print(picdir)     
-
+import requests
 import logging
 from waveshare_epd import epd2in13_V4
 import time
 from datetime import datetime
 from PIL import Image,ImageDraw,ImageFont
+from pprint import pprint
 
+settings = {
+    'api_key':'80c36effec1454b7c7a9c2570f6ac995',
+    'zip_code':'60625',
+    'country_code':'us',
+    'temp_unit':'imperial'} #unit can be metric, imperial, or kelvin
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid={0}&zip={1},{2}&units={3}"
+final_url = BASE_URL.format(settings["api_key"],settings["zip_code"],settings["country_code"],settings["temp_unit"])
+
+# Map the OpenWeatherMap icon code to the appropriate font character
+# See http://www.alessioatzeni.com/meteocons/ for icons
+ICON_MAP = {
+    "01d": "B",
+    "01n": "C",
+    "02d": "H",
+    "02n": "I",
+    "03d": "N",
+    "03n": "N",
+    "04d": "Y",
+    "04n": "Y",
+    "09d": "Q",
+    "09n": "Q",
+    "10d": "R",
+    "10n": "R",
+    "11d": "Z",
+    "11n": "Z",
+    "13d": "W",
+    "13n": "W",
+    "50d": "J",
+    "50n": "K",
+}
+
+font76 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 76)   
+font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
+icon_font = ImageFont.truetype("meteocons.ttf", 30)
+    
 logging.basicConfig(level=logging.DEBUG)
 
 epd = epd2in13_V4.EPD()
 
 try:
-    font76 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 76)   
-    font72 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 72)    
-    font52 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 52)
-    font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
     print(epd.height)
     print(epd.width)
     logging.info("init and Clear")
@@ -41,18 +74,32 @@ try:
     time_draw = ImageDraw.Draw(time_image)
     epd.displayPartBaseImage(epd.getbuffer(time_image2))
     start_hour = datetime.now().hour
+    
+    # Get the weather data for the first time
+    weather_data = requests.get(final_url).json()
+    pprint(weather_data)    # print to console just for information
+    temperature = int(weather_data["main"]["temp"]) # temperature in Fahrenheit
+    weather_icon = ICON_MAP[weather_data["weather"][0]["icon"]] # weather icon code
         
     while (True):
           
+        # Refresh the display every hour  
         if (start_hour != datetime.now().hour):
             start_hour = datetime.now().hour
             epd.init()
             epd.Clear(0xFF)
             epd.displayPartBaseImage(epd.getbuffer(time_image2))
+        
+        #update weather every 10 minutes
+        if (datetime.now().minute % 10 == 0):
+            weather_data = requests.get(final_url).json()
+            temperature = int(weather_data["main"]["temp"])
+            weather_icon = ICON_MAP[weather_data["weather"][0]["icon"]]
             
         time_draw.rectangle((0, 0, epd.height, epd.width), fill = 255)
         time_draw.text((125, 50), datetime.now().strftime('%H:%M'), font = font76, fill = 0, anchor="mm")
-        time_draw.text((125, 122), datetime.now().strftime('%a, %d %B'), font = font30, fill = 0, anchor="md")
+        time_draw.text((125, 122), datetime.now().strftime('%d/%m') + " - " + str(temperature) + "°F", font = font30, fill = 0, anchor="md")
+        time_draw.text((240, 122), weather_icon, font = icon_font, fill = 0, anchor="rd")
         epd.displayPartial(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
         
         now = datetime.now()
